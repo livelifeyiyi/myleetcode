7a71cafb124a534f60df569176fe0cb543bbb04e
livelifeyiyi <xiaoya_0922@126.com>
2016-02-27 20:49:00 +0800

    substring,unfinished

---
+++ 3.subsing(unfinished).py
@@ -0,0 +1,11 @@
+#class Solution(object):
+#    def lengthOfLongestSubstring(self, s):
+#        """
+#        :type s: str
+#        :rtype: int
+#        """
+
+s =  "abcabcbb"
+arr = []
+arr = s[:]
+print arr[0]
