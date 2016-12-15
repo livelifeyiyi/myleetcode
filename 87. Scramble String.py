a4459fad64899c58a088c3db1f8c3955151935ac
livelifeyiyi <xiaoya_0922@126.com>
2016-12-15 11:50:15 +0800

    use dfs method, compare left and right part seperately

---
+++ 87. Scramble String.py
@@ -0,0 +1,32 @@
+class Solution(object):
+	def isScramble(self, s1, s2):
+		"""
+		:type s1: str
+		:type s2: str
+		:rtype: bool
+		"""
+		
+		def getScramble(s1, s2, res):
+			if (s1,s2) in res.keys():
+				return res[(s1,s2)]
+			if not sorted(s1) == sorted(s2):
+				return False
+			if len(s1) == 1:
+				return s1 == s2
+			for i in range(1, len(s1)):
+				if getScramble(s1[:i], s2[-i:], res) and getScramble(s1[i:], s2[:-i], res) or getScramble(s1[:i], s2[:i], res) and getScramble(s1[i:], s2[i:], res):
+					res[(s1,s2)] = True
+					return True
+			res[(s1,s2)] = False
+			return False
+		res = {}
+		return getScramble(s1, s2, res)
+		"""l, r = 0, len(s1)
+		mid = (l+r) / 2
+		left = s1[l: mid][::-1]
+		right = s1[mid: r][::-1]
+		res += getScramble(left, res)
+		res += getScramble(right, res)"""
+		
+S =Solution()
+print S.isScramble('great','rgtae')
